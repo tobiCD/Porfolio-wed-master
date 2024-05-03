35d66ea3eed770c51f8eaee3dfee0fbaf85261cd
Chu Đăng Khôi <106723115+tobiCD@users.noreply.github.com>
2024-05-03 15:11:20 +0700

    Update settings.py

--- porfolio2/settings.py
+++ porfolio2/settings.py
@@ -110,6 +110,10 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+DATABASES['default'] = dj_database_url.config(
+    conn_max_age=600,
+    conn_health_checks=True,
+)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
